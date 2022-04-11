46cbf4cc25eaf92cc4fc98d44c25d69ccb167e2a
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-04-12 01:17:30 +0530

    12/04/22

---
+++ 01.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+//This program was created by Fenil Khunt.
+
+/*
+This 
+is 
+multi 
+line 
+comment
+*/
+
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
---
+++ 02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// If you want to add comment to slected lines then there is a shorcut in vs-code
+// frist you have to select the lines then "ctrl + /"
+
+int main()
+{
+    int sum = 6;
+    cout << "Hello World " << sum;
+    return 0;
+}
