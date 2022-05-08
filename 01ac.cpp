9fe85684be9950c6ea80b91b894d661b95b21c43
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-05-08 22:47:44 +0530

    01*08/05/22

---
+++ 01ac.cpp
@@ -0,0 +1,18 @@
+//Datatypes
+
+#include<iostream>
+using namespace std;
+
+ int main()
+ {
+    int a; //variable declaation
+    a = 12; //initialisation 
+    cout<<"size of int "<<sizeof(a)<<endl;
+    
+    float b;
+    b = 0.1245;
+    cout<<"size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of char "<<sizeof(c)<<endl;
+ }
