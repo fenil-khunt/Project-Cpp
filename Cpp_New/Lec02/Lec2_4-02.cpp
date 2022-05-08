e4779b7a02d2cae6a8e7a4c35aadb735d64567a6
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-05-09 01:10:08 +0530

    05*09/05/22

---
+++ Cpp_New/Lec02/Lec2_4-02.cpp
@@ -0,0 +1,18 @@
+//Introduction to While Loops
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    while(n>0)
+    {
+        cout<<n<<endl;
+        cin>>n;
+    }
+    
+    return 0;
+}
