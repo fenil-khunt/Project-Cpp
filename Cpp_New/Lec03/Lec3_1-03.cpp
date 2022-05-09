07475c26137df19d0bb3df96294381404ff6811b
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-05-10 00:40:53 +0530

    03*10/05/22

---
+++ Cpp_New/Lec03/Lec3_1-03.cpp
@@ -0,0 +1,28 @@
+// data structure and algorithms
+// prime or not
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int i;
+
+    for(i=2; i<n; i++)
+    {
+        if(n%i==0)
+        {
+            cout<<"Non Prime"<<endl;
+            break;        
+        }
+    }
+
+    if (i==n)
+    {
+        cout<<"Prime"<<endl;
+    }
+    
+    return 0;
+}
