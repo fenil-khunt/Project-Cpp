ec156d0b02432f5f2343b2121ef8fd3a8c9473f9
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-05-10 00:19:34 +0530

    01*10/05/22

---
+++ Cpp_New/Lec03/Lec3_1-01.cpp
@@ -0,0 +1,27 @@
+// data structure and algorithms
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int PoketMoney = 3000;
+    for(int date=1; date<=30; date++)
+    {
+        if(date%2==0)
+        {
+            continue;
+        }
+
+        if(PoketMoney==0)
+        {
+            break;
+        }
+
+        cout<<date<<" : Go out Today !"<<endl;
+        PoketMoney = PoketMoney - 300;
+    }
+    
+    
+    return 0;
+}
