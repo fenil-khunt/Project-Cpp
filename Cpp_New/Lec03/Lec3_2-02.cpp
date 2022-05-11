f2783a668d2b84912a6d9b89369e033cbed90a7e
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-05-11 19:41:55 +0530

    03*11/05/22

---
+++ Cpp_New/Lec03/Lec3_2-02.cpp
@@ -0,0 +1,40 @@
+// impliment a simple calculator using switch 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+    cout<<"Input two numbers : ";
+    cin>>n1>>n2;
+
+    char op;
+    cout<<"Inpur oprrator : ";
+    cin>>op;
+    
+    switch (op)
+    {
+    case '/':
+        cout<<n1/n2<<endl;
+        break;
+    
+    case '*':
+        cout<<n1*n2<<endl;
+        break;
+
+    case '-':
+        cout<<n1-n2<<endl;
+        break;
+
+    case '+':
+        cout<<n1+n2<<endl;
+        break;
+
+    default:
+        cout<<"Enter valid oprator,/n "<<endl;
+        break;
+    }
+    
+    return 0;
+}
