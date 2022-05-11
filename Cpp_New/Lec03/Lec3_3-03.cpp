45a87adb648785120d3ccac29b7b3323e2a2c1c7
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-05-12 01:09:00 +0530

    03*12/05/22

---
+++ Cpp_New/Lec03/Lec3_3-03.cpp
@@ -0,0 +1,19 @@
+// oprators 
+// pre-post increament-decreament
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+
+//      1     0      1     0         after oprator velue
+    i = i++ + i-- + ++i + --i ;
+//      0     1      1     0         at a time oprator velue
+    
+    cout<<"i = "<<i<<endl; 
+    
+    
+    return 0;
+}
