755df8a0b11ccdf092bcc2365d59fc42ee2f2fe4
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-07-13 00:08:56 +0530

    02*01/05/22

---
+++ Cpp_New/Lec04/Lec4_1-02.cpp
@@ -0,0 +1,30 @@
+// Hollow Rectangular Pattern
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int row,colm;
+    cin>>row>>colm;
+
+    for(int i=1; i<=row; i++)
+    {
+        for(int j=1; j<=colm; j++)
+        {
+            if(i==1 || i==row || j==1 || j==colm)
+            {
+                cout<<"*";
+            }
+            
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+        
+    }
+    
+    return 0;
+}
