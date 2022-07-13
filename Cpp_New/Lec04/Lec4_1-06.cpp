860bfbde0a6fcac909c08ff5ff609bba06da4346
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-07-13 14:37:16 +0530

    06*13/07/22

---
+++ Cpp_New/Lec04/Lec4_1-06.cpp
@@ -0,0 +1,22 @@
+// Floyd's Triangle
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int num=1,row=1; row<=n; row++)
+    {
+        for(int colm=1; colm<=row; colm++,num++)
+        {
+            cout<<num<<" ";
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
