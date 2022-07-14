e2ac0f688b7e73547f7387b1ed0c1d3ab6f72176
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-07-15 02:02:09 +0530

    01*14/07/22

--- Cpp_New/Lec04/Lec4_1-06.cpp
+++ Cpp_New/Lec04/Lec4_1-06.cpp
@@ -16,7 +16,6 @@ int main()
         }
         cout<<endl;
     }
-    
 
     return 0;
 }
---
+++ Cpp_New/Lec04/Lec4_1-07.cpp
@@ -0,0 +1,55 @@
+// Butterfly Pattern
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int num=(2*n);
+
+    cin>>n;
+
+    for(int num=(2*n),row=1; row<=n; row++,num--)
+    {
+        for(int colm=1; colm<=(2*n); colm++)
+        {
+            if(colm<=row)
+            {
+                cout<<"*";
+            }
+            else if(num<=colm)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    for(int num=n,row=n; row>=1; row--,num++)
+    {
+        for(int colm=1; colm<=(2*n); colm++)
+        {
+            if(colm<=row)
+            {
+                cout<<"*";
+            }
+            else if(num<colm)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    
+    
+    return 0;
+}
