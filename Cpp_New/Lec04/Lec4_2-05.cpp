3809e3a9488f7d36dc632a18f41a2b4c8ec6a01d
Fenil Khunt <97497591+fenil-khunt@users.noreply.github.com>
2022-07-19 01:59:38 +0530

    02*19/07/22

---
+++ Cpp_New/Lec04/Lec4_2-05.cpp
@@ -0,0 +1,35 @@
+// Palindromic Pattern
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int row=1; row<=n; row++)
+    {
+        for(int colm=n; colm>=1; colm--)
+        {            
+            if(colm<=row)
+            {
+                for(int num=row; 1<=num; num--,colm--)
+                {
+                    cout<<num<<" ";
+                }
+                for(int num=2; num<=row; num++,colm--)
+                {
+                    cout<<num<<" ";
+                }
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
